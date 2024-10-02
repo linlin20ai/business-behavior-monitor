7077a31aa82a03d1adeb7681cc0f493e1c0109d2
linlin20ai <3357929671@qq.com>
2024-10-02 11:10:45 +0800

    admin init

---
+++ business-behavior-monitor-admin/src/main/java/cn/xiaohamigua/monitor/Application.java
@@ -0,0 +1,18 @@
+package cn.xiaohamigua.monitor;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author 林圣涛
+ */
+@SpringBootApplication
+@Configurable
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
